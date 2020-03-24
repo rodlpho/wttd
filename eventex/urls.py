bad90618aefe05a85788bf2ffa0963218c2fbbba
Rodolpho <rodolphodeales@gmail.com>
2020-03-24 20:16:59 -0300

    Remove comments on urls.py file

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-#from django.urls import path
-
-#import eventex.core.views
 from eventex.core.views import home
 
 urlpatterns = [
