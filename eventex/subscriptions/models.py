6efb858bad4fd01ed38a84c7a62d71025de83c5f
Rodolpho <rodolphodeales@gmail.com>
2020-05-16 17:50:17 -0300

    Tenta corrigir erros da M4A11

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -14,6 +14,7 @@ class Subscription(models.Model):
     phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
+    hash_url = models.CharField('URL', max_length=32, null=True)
 
     def __str__(self):
         return self.name
