92a71b396eb233c81e56b9757a5e2ee3e5539316
Rodolpho <rodolphodeales@gmail.com>
2020-05-29 14:03:35 -0300

    Corrige migracao para Heroku

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -14,9 +14,7 @@ class Subscription(models.Model):
     phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
-    #hash_url = models.CharField('URL', max_length=, null=True)
-    hash_url = models.UUIDField(primary_key=False, default=uuid.uuid1().hex, editable=True)
-
+    hash_url = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True)
 
     def __str__(self):
         return self.name
